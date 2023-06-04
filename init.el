90e1fc91680f8ae7a99836e26d2c3af6eace279e
Josh Rickmar <opensource@zettaport.com>
2023-06-04 19:08:56 +0000

    disable tab indents by default, reformat config with spaces

--- init.el
+++ init.el
@@ -13,24 +13,25 @@
   :init
   (setq lsp-keymap-prefix "C-c l")
   :bind (:map lsp-mode-map
-	      ("C-c d" . lsp-describe-thing-at-point)
-	      ("C-c a" . lsp-execute-code-action))
+              ("C-c d" . lsp-describe-thing-at-point)
+              ("C-c a" . lsp-execute-code-action))
   :config
   (lsp-enable-which-key-integration t))
 
 (use-package company
   :config
   (setq company-minimum-prefix-length 1
-	company-idle-delay 0.0))
+        company-idle-delay 0.0))
 
 (use-package go-mode
   :init
   (defun my-go-compile ()
     (set (make-local-variable 'compile-command)
-	 "go test"))
+         "go test"))
+  (setq indent-tabs-mode t)
   :hook ((go-mode . lsp-deferred)
-	 (go-mode . my-go-compile)
-	 (before-save . gofmt-before-save)))
+         (go-mode . my-go-compile)
+         (before-save . gofmt-before-save)))
 
 (use-package racket-mode)
 
@@ -44,8 +45,8 @@
 ;; keybindings
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "C-c C-t") (lambda ()
-				  (interactive)
-				  (ansi-term "/bin/ksh")))
+                                  (interactive)
+                                  (ansi-term "/bin/ksh")))
 (global-set-key (kbd "C-c C-v") 'flymake-goto-next-error)
 (global-set-key (kbd "C-c m") 'man)
 (global-set-key (kbd "M-z") 'zap-up-to-char)
@@ -61,9 +62,10 @@
 (set-fringe-mode 5)
 (tool-bar-mode 0)
 (setq-default cursor-type 'bar
-	      fill-column 80
-	      show-trailing-whitespace t
-	      word-wrap t)
+              fill-column 80
+              indent-tabs-mode nil
+              show-trailing-whitespace t
+              word-wrap t)
 (setq inhibit-startup-message t
       mouse-autoselect-window t
       mouse-wheel-scroll-amount '(10)
@@ -76,25 +78,25 @@
       inferior-lisp-program "sbcl")
 
 (dolist (hook '(org-mode-hook
-		term-mode-hook
-		shell-mode-hook
-		eshell-mode-hook
-		racket-repl-mode
-		slime-repl-mode))
+                term-mode-hook
+                shell-mode-hook
+                eshell-mode-hook
+                racket-repl-mode
+                slime-repl-mode))
   (add-hook hook (lambda ()
-		   (display-line-numbers-mode nil)
-		   (setq show-trailing-whitespace nil))))
+                   (display-line-numbers-mode nil)
+                   (setq show-trailing-whitespace nil))))
 
 ;; backups
 (setq backup-directory-alist `(("." . ,(concat user-emacs-directory
-					       "backups"))))
+                                               "backups"))))
 
 ;; theme
 (set-face-attribute 'default nil
-		    :font "Go Mono"
-		    :height 105
-		    :background "#fff2ff"
-		    :foreground "#180818")
+                    :font "Go Mono"
+                    :height 105
+                    :background "#fff2ff"
+                    :foreground "#180818")
 (set-face-attribute 'mode-line nil :background "#edd8ed" :box nil)
 (set-face-attribute 'mode-line-inactive nil :foreground "#680048" :box nil)
 (custom-set-faces
